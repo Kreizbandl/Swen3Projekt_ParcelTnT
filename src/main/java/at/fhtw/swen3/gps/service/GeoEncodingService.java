2d6a6e23efd2794f7805aaea7581eba49ac941f8
if19b020 <if19b020@technikum-wien.at>
2022-12-10 22:48:18 +0100

    update geoencoding

--- src/main/java/at/fhtw/swen3/gps/service/GeoEncodingService.java
+++ src/main/java/at/fhtw/swen3/gps/service/GeoEncodingService.java
@@ -1,4 +1,9 @@
 package at.fhtw.swen3.gps.service;
 
+import at.fhtw.swen3.persistence.entities.RecipientEntity;
+import at.fhtw.swen3.services.dto.GeoCoordinate;
+
+
 public interface GeoEncodingService {
+    GeoCoordinate encodeAddress(RecipientEntity recipient);
 }
--- src/main/java/at/fhtw/swen3/gps/service/impl/BindEncodingProxy.java
+++
@@ -1,6 +0,0 @@
-package at.fhtw.swen3.gps.service.impl;
-
-import at.fhtw.swen3.gps.service.GeoEncodingService;
-
-public class BindEncodingProxy implements GeoEncodingService {
-}
